2516d56bb839d43aeaa45bb86dff16dc7e9046d0
justim <justim@gmail.com>
2017-12-12 16:10:12 +0800

    將找最大值的程式碼，改用 CPP 提供的 max_element 取代 求兩個數值的 min 值，也用 CPP min 函式取代。如此程式碼可以更精簡

--- C_CH13_坑洞路面/Source.cpp
+++ C_CH13_坑洞路面/Source.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main()
 {
@@ -14,29 +15,13 @@ int main()
 	for (int pos = 1; pos < n - 1; pos++)
 	{
 		//檢查左邊最大值
-		int lmax = 0;
-		for (int j = 0; j <= pos; j++)
-		{
-			if (arr[j] > lmax)
-			{
-				lmax = arr[j];
-			}
-		}
+		int lmax = *max_element(arr, arr + pos + 1);
+		
 		//檢查右邊最大值
-		int rmax = 0;
-		for (int j = pos; j < n; j++)
-		{
-			if (arr[j] > rmax)
-			{
-				rmax = arr[j];
-			}
-		}
-		int minmax = 0;
-		if (rmax > lmax)
-			minmax = lmax;
-		else
-			minmax = rmax;
-		//sum = sum + (minmax - arr[pos]);
+		int rmax = *max_element(arr + pos, arr + n);
+		
+		int minmax = min(rmax, lmax);
+				
 		sum += minmax - arr[pos];
 	}
 	cout << sum << endl;
